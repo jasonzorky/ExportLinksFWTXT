8b11b4a20f89baa094bb66074ae4c07ce8875f94
jasonzorky <109059268+jasonzorky@users.noreply.github.com>
2023-05-12 21:13:43 -0300

    Update ExportarLinksEmTXTnaFW.js

--- ExportarLinksEmTXTnaFW.js
+++ ExportarLinksEmTXTnaFW.js
@@ -1,10 +1,13 @@
 // ==UserScript==
 // @name         Exportar Links em TXT na FW
-// @namespace    http://tampermonkey.net/
+// @namespace    JasonZorky
 // @version      1.0
 // @description  Exporta links em TXT da div 'upload_links' na página 'https://filewarez.tv/showthread.php?t=*', ignorando os links que começam com 'vlc://'
 // @author       JasonZorky
 // @match        https://filewarez.tv/showthread.php?t=*
+// @match        https://raw.githubusercontent.com/*
+// @updateURL    https://raw.githubusercontent.com/jasonzorky/ExportLinksFWTXT/main/ExportarLinksEmTXTnaFW.js
+// @downloadURL  https://raw.githubusercontent.com/jasonzorky/ExportLinksFWTXT/main/ExportarLinksEmTXTnaFW.js
 // @grant        none
 // ==/UserScript==
 
